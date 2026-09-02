18bfd1307e55fc4639532e2d26149a95e81c74c7
Chetan Sharma <161132484+chetanschetan@users.noreply.github.com>
2026-09-02 20:55:54 +0530

    Time: 173 ms (22.36%) | Memory: 42.9 MB (59.85%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,15 +1,11 @@
 class Solution {
     int[] rowDir = {-1,1,0,0};
     int[] colDir = {0,0,-1,1};
-
     public boolean exist(char[][] board, String word) {
-        int row = board.length;
-        int col = board[0].length;
-        boolean vis[][] = new boolean[row][col];
-
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(solve(board, i, j, word, 0, vis)){
+        boolean[][] vis = new boolean[board.length][board[0].length];
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(solve(board, word, i, j, 0, vis)){
                     return true;
                 }
             }
@@ -17,30 +13,32 @@ class Solution {
         return false;
     }
 
-    public boolean solve(char[][] board, int row, int col, String word, int index, boolean vis[][]){
+    public boolean solve(char[][] board, String word, int row, int col, int index, boolean[][] vis){
         if(index == word.length()){
             return true;
         }
+
+
         if(board[row][col] != word.charAt(index)) return false;
-        // to check if current character is last character of word
-        if(index == word.length()-1){
-            return true;
-        }
+
+        if(index == word.length() - 1) return true;
 
         vis[row][col] = true;
+
 
         for(int i=0;i<4;i++){
             int newRow = row + rowDir[i];
             int newCol = col + colDir[i];
 
-            if(newRow>=0 && newRow<board.length && newCol>=0 && newCol<board[0].length && vis[newRow][newCol] == false && index+1 < word.length()
-            && board[newRow][newCol] == word.charAt(index+1)){
-                if(solve(board, newRow, newCol, word, index+1, vis)){
-                    return true;
-                }
+            if(newRow>=0 && newRow<board.length && newCol >= 0 && newCol < board[0].length &&
+            vis[newRow][newCol] == false && index+1<word.length() && board[newRow][newCol] == word.charAt(index+1)){
+                 if(solve(board, word, newRow, newCol, index+1, vis))
+                return true;
             }
         }
+
         vis[row][col] = false;
+
         return false;
     }
 }
