7778dbfd252869bd4e2ae6326dfa21cd708d1b65
Chetan Sharma <161132484+chetanschetan@users.noreply.github.com>
2026-09-03 08:33:20 +0530

    Time: 131 ms (34.00%) | Memory: 52.7 MB (66.38%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.java
+++ 282-expression-add-operators/expression-add-operators.java
@@ -1,11 +1,11 @@
 class Solution {
     public List<String> addOperators(String num, int target) {
         List<String> res = new ArrayList<>();
-        solve(num, target, 0, 0, 0, res, "");
+        solve(num, target, 0, 0, 0, "", res);
         return res;
     }
-    
-    public void solve(String num, int target, int index, long total, long prev, List<String> res, String exp){
+
+    public void solve(String num, int target, int index, long total, long prev, String exp, List<String> res){
         if(index == num.length()){
             if(total == target){
                 res.add(exp);
@@ -15,19 +15,18 @@ class Solution {
 
         for(int end=index;end<num.length();end++){
             String newStr = num.substring(index, end+1);
+            long currNum = Long.parseLong(newStr);
 
-            if(newStr.length()>1 && newStr.charAt(0) == '0'){
+            if(newStr.length() > 1 && newStr.charAt(0) == '0'){
                 break;
             }
 
-            long currNum = Long.parseLong(newStr);
-
-            if(index == 0){
-                solve(num, target, end+1, currNum, currNum, res, exp+newStr);
+            if(index==0){
+                solve(num, target, end+1, currNum, currNum, exp+newStr, res);
             }else{
-                solve(num, target, end+1, total + currNum, currNum, res, exp+"+"+newStr);
-                solve(num, target, end+1, total - currNum, -currNum, res, exp+"-"+newStr);
-                solve(num, target, end+1, (total - prev) + (currNum * prev), prev*currNum, res, exp+"*"+newStr);
+                solve(num, target, end+1, total+currNum, currNum, exp+"+"+newStr, res);
+                solve(num, target, end+1, total-currNum, -currNum, exp+"-"+newStr, res);
+                solve(num, target, end+1, (total-prev) + (currNum * prev), currNum*prev, exp+"*"+newStr, res);
             }
         }
     }
